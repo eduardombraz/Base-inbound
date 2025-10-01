c8f4189f9865b5c1463289a832538691e06b3ca0
eduardombraz <eduardo.louzada@shopee.com>
2025-10-01 14:52:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -82,11 +82,11 @@ async def main():
             await page.goto("https://spx.shopee.com.br/#/hubLinehaulTrips/trip")
             await page.wait_for_timeout(8000)
             await page.locator(
-                'xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/span[1]'
+                'xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/span[1]'
             ).click()
             await page.wait_for_timeout(8000)
             await page.get_by_role("button", name="Exportar").nth(0).click()
-            await page.wait_for_timeout(80000)
+            await page.wait_for_timeout(200000)
 
             # 👉 Botão de download 1
             await page.goto("https://spx.shopee.com.br/#/taskCenter/exportTaskCenter")
@@ -97,7 +97,6 @@ async def main():
             download_path = os.path.join(DOWNLOAD_DIR, download.suggested_filename)
             await download.save_as(download_path)
             new_file_path = rename_downloaded_file(DOWNLOAD_DIR, download_path)
-
             #####################################################################
 
             # Atualizar Google Sheets
