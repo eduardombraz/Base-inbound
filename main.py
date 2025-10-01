2d5df91ab4ba8de9765b6a30ae6cc6e559c33bd8
eduardombraz <eduardo.louzada@shopee.com>
2025-10-01 15:21:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -81,20 +81,18 @@ async def main():
             # NAVEGAÇÃO E DOWNLOAD 1
             await page.goto("https://spx.shopee.com.br/#/hubLinehaulTrips/trip")
             await page.wait_for_timeout(8000)
-            await page.locator(
-                'xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/span[1]'
-            ).click()
-            await page.wait_for_timeout(8000)
             await page.get_by_role("button", name="Exportar").nth(0).click()
             await page.wait_for_timeout(10000)
 
+            # 👉 Botão de download 1
+            await page.goto("https://spx.shopee.com.br/#/taskCenter/exportTaskCenter")
+            await page.wait_for_timeout(8000)
             async with page.expect_download() as download_info:
-                await page.get_by_role("button", name="Baixar").first.click()
-            
+                await page.get_by_role("button", name="Baixar").nth(0).click()
             download = await download_info.value
             download_path = os.path.join(DOWNLOAD_DIR, download.suggested_filename)
             await download.save_as(download_path)
-            print(f"Download concluído: {download_path}")
+            new_file_path = rename_downloaded_file(DOWNLOAD_DIR, download_path)
 
             #####################################################################
 
